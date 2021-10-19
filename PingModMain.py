ddd97b8baa64dd2a4cb94fe382b13c1db33edacd
Ben <benjx32@gmail.com>
2021-10-19 12:02:00 -0700

    scrapped C++ stuff

--- PingModMain.py
+++ PingModMain.py
@@ -1,18 +1,22 @@
+from logging import _Level
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_guild_permissions, has_permissions
-
 
 import os
 import sys
 import string
 
-
-import ctypes
-
-
 from dotenv import load_dotenv
 load_dotenv()
+
+global mode
+global hostility
+global tolerance
+
+mode = 0
+hostility = 1
+tolerance = 5
 
 bot = commands.Bot(command_prefix = '!')
 '''
@@ -21,16 +25,27 @@ async def on_message(ctx):
     if bot.user == ctx.author: return
     await ctx.channel.send('test')
 '''
+
 @bot.command()
 @has_permissions(administrator = True)
 async def config(ctx, setting, level):
-    await ctx.reply('Changing hostility level from' )
-    return
+    global tolerance
+    global hostility
+    global mode
+    if str(setting) == "mode":
+        await ctx.reply(f'Changing {setting} level from {mode} to {level}')
+        mode = level
+    elif str(setting) == "hostility":
+        await ctx.reply(f'Changing {setting} level from {hostility} to {level}')
+        hostility = level
+    elif str(setting) == "tolerance":
+        await ctx.reply(f'Changing {setting} level from {tolerance} to {level}')
+        tolerance = level
 
 @bot.event
-async def on_message(ctx, user:discord.Member = None):
+async def on_message(ctx):
     await bot.process_commands(ctx)
     if bot.user == ctx.author: return
     print("t")
-
+    
 bot.run(os.getenv('TOKEN'))
