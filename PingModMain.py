18fe66a569b45ca0be50ac85b687267a560cfa47
Ben <benjx32@gmail.com>
2021-11-04 22:43:25 -0700

    apparently discord.py is dead, just gonna leave it barebones functional for small servers

--- PingModMain.py
+++ PingModMain.py
@@ -9,32 +9,13 @@ import string
 from dotenv import load_dotenv
 load_dotenv()
 
-from serverconfig import serverconfig
-
 bot = commands.Bot(command_prefix = '!')
-activeconfig = serverconfig(0, 1, 5)
-
-
-@bot.command()
-@has_permissions(administrator = True)
-async def config(ctx, setting, level):
-    if str(setting) == "mode":
-        await ctx.reply(f'Changing {setting} level from {activeconfig.mode} to {level}')
-        activeconfig.setMode(level)
-    elif str(setting) == "hostility":
-        await ctx.reply(f'Changing {setting} level from {activeconfig.hostility} to {level}')
-        activeconfig.setHostility(level)
-    elif str(setting) == "maxwarnings":
-        await ctx.reply(f'Changing {setting} level from {activeconfig.maxwarnings} to {level}')
-        activeconfig.setMaxwarnings(level)
-    else: await ctx.reply('Error: Invalid Setting, Only "mode" (0-2), "hostility" (1-3), "maxwarnings" (0+) are valid arguments. ')
 
 @bot.event
 async def on_message(ctx):
     await bot.process_commands(ctx)
     if bot.user == ctx.author: return
     if str(ctx.content).find('@everyone')!=-1 or str(ctx.content).find('@here')!=-1:
-        await ctx.channel.send("SHUT UP")
-    #print(ctx.author.id)
+        await ctx.channel.send(f'Shut the fuck up {ctx.author.mention}')
 
 bot.run(os.getenv('TOKEN'))
